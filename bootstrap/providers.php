e84c2bc64ed53d1209691f5e18ad598a0c4162bc
Zumix26 <levanovn36@gmail.com>
2024-01-15 10:00:00 +0300

    Установка Laravel

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    App\Infrastructure\Providers\AppServiceProvider::class,
+    App\Infrastructure\Providers\DomainServiceProvider::class,
+    App\Infrastructure\Providers\EventServiceProvider::class,
+];
